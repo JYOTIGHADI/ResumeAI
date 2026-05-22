1011fc40d0df1dea20ba38e483846dce617b37d2
basic-html.css <basic-html.css.in>
2026-05-22 15:56:30 +0530

    update

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://resumeai-backend-zec5.onrender.com",
+    baseURL: "https://resumeai-backend-1pm4.onrender.com",
     withCredentials: true
 })
 
