bc6637223892f6a9fc3088ff5ccedfdd4e09c16b
basic-html.css <basic-html.css.in>
2026-05-22 19:02:49 +0530

    UPDATE

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://resumeai-backend-of01.onrender.com",
+    VITE_API_URL: import.meta.env.VITE_API_URL || "http://localhost:3000",
     withCredentials: true,
 })
 
