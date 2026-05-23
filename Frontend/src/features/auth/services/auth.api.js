1e6cee31b489a5a5256a1ba538002ce546717c3b
basic-html.css <basic-html.css.in>
2026-05-23 10:36:39 +0530

    fix api

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -67,111 +67,176 @@
 
 
 
+// import axios from "axios"
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL,
+//     withCredentials: true
+// })
+
+// /**
+//  * REGISTER
+//  */
+// export async function register({
+//     username,
+//     email,
+//     password
+// }) {
+
+//     try {
+
+//         const response = await api.post(
+//             "/api/auth/register",
+//             {
+//                 username,
+//                 email,
+//                 password
+//             }
+//         )
+
+//         return response.data
+
+//     } catch (err) {
+
+//         console.log(err)
+
+//         throw err
+//     }
+// }
+
+// /**
+//  * LOGIN
+//  */
+// export async function login({
+//     email,
+//     password
+// }) {
+
+//     try {
+
+//         const response = await api.post(
+//             "/api/auth/login",
+//             {
+//                 email,
+//                 password
+//             }
+//         )
+
+//         return response.data
+
+//     } catch (err) {
+
+//         console.log(err)
+
+//         throw err
+//     }
+// }
+
+// /**
+//  * LOGOUT
+//  */
+// export async function logout() {
+
+//     try {
+
+//         const response = await api.get(
+//             "/api/auth/logout"
+//         )
+
+//         return response.data
+
+//     } catch (err) {
+
+//         console.log(err)
+
+//         throw err
+//     }
+// }
+
+// /**
+//  * GET CURRENT USER
+//  */
+// export async function getMe() {
+
+//     try {
+
+//         const response = await api.get(
+//             "/api/auth/get-me"
+//         )
+
+//         return response.data
+
+//     } catch (err) {
+
+//         console.log(err)
+
+//         throw err
+//     }
+// }
+
+// export default api
+
+
 import axios from "axios"
 
+const API_URL =
+    "https://resumeai-backend-of01.onrender.com"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_URL,
     withCredentials: true
 })
 
-/**
- * REGISTER
- */
 export async function register({
     username,
     email,
     password
 }) {
 
-    try {
+    const response = await api.post(
+        "/api/auth/register",
+        {
+            username,
+            email,
+            password
+        }
+    )
 
-        const response = await api.post(
-            "/api/auth/register",
-            {
-                username,
-                email,
-                password
-            }
-        )
-
-        return response.data
-
-    } catch (err) {
-
-        console.log(err)
-
-        throw err
-    }
+    return response.data
 }
 
-/**
- * LOGIN
- */
 export async function login({
     email,
     password
 }) {
 
-    try {
+    const response = await api.post(
+        "/api/auth/login",
+        {
+            email,
+            password
+        }
+    )
 
-        const response = await api.post(
-            "/api/auth/login",
-            {
-                email,
-                password
-            }
-        )
-
-        return response.data
-
-    } catch (err) {
-
-        console.log(err)
-
-        throw err
-    }
+    return response.data
 }
 
-/**
- * LOGOUT
- */
 export async function logout() {
 
-    try {
+    const response = await api.get(
+        "/api/auth/logout"
+    )
 
-        const response = await api.get(
-            "/api/auth/logout"
-        )
-
-        return response.data
-
-    } catch (err) {
-
-        console.log(err)
-
-        throw err
-    }
+    return response.data
 }
 
-/**
- * GET CURRENT USER
- */
 export async function getMe() {
 
-    try {
+    const response = await api.get(
+        "/api/auth/get-me"
+    )
 
-        const response = await api.get(
-            "/api/auth/get-me"
-        )
-
-        return response.data
-
-    } catch (err) {
-
-        console.log(err)
-
-        throw err
-    }
+    return response.data
 }
 
 export default api
